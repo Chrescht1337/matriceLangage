ec610fb4d02ff579b68e47e2e8acab33ed0fb5fe
Chrescht1337 <frantzenchristian@hotmail.com>
2014-12-08 02:41:40 +0100

    slice classe before sleep

--- sliceMatrix.cpp
+++ sliceMatrix.cpp
@@ -13,11 +13,11 @@ sliceMatrix<Elem,dim,callsLeft>::sliceMatrix(Matrix<Elem,dim>& Mat_):Mat(Mat)
 }*/
 
 template<typename Elem,std::size_t dim>
-sliceMatrix<Elem,dim>::sliceMatrix(Matrix<Elem,dim>& Mat_,std::initializer_list<std::pair<std::size_t,std::size_t>> restrictions_) : Mat(Mat_)
+sliceMatrix<Elem,dim>::sliceMatrix(Matrix<Elem,dim>& Mat_,std::initializer_list<std::pair<std::ptrdiff_t,std::ptrdiff_t>> restrictions_) : Mat(Mat_)
 {
     if (this->validRestrictions(restrictions_))
     {
-        restrictions= new std::pair<std::size_t,std::size_t>[dim];
+        restrictions= new std::pair<std::ptrdiff_t,std::ptrdiff_t>[dim];
         std::size_t i=0;
         for (auto vals:restrictions_)
         {
@@ -26,20 +26,20 @@ sliceMatrix<Elem,dim>::sliceMatrix(Matrix<Elem,dim>& Mat_,std::initializer_list<
         }
         steps= new std::size_t[dim];
         for (std::size_t j=0;j<dim;j++)
-            steps[j]=0;
+            steps[j]=1;
     }
     else
         throw std::invalid_argument("Incompatible boundaries");
 }
 
 template<typename Elem,std::size_t dim>
-sliceMatrix<Elem,dim>::sliceMatrix(Matrix<Elem,dim>& Mat_,std::initializer_list<std::pair<std::size_t,std::size_t>> restrictions_,std::initializer_list<std::size_t> steps_ ): Mat(Mat_)
+sliceMatrix<Elem,dim>::sliceMatrix(Matrix<Elem,dim>& Mat_,std::initializer_list<std::pair<std::ptrdiff_t,std::ptrdiff_t>> restrictions_,std::initializer_list<std::size_t> steps_ ): Mat(Mat_)
 {
     if (this->validRestrictions(restrictions_))
     {
         if (this->validSteps(steps_))
         {
-            restrictions= new std::pair<std::size_t,std::size_t>[dim];
+            restrictions= new std::pair<std::ptrdiff_t,std::ptrdiff_t>[dim];
             std::size_t i=0;
             for (auto vals:restrictions_)
             {
@@ -62,7 +62,7 @@ sliceMatrix<Elem,dim>::sliceMatrix(Matrix<Elem,dim>& Mat_,std::initializer_list<
 
 
 template<typename Elem,std::size_t dim>
-bool sliceMatrix<Elem,dim>::validRestrictions(std::initializer_list<std::pair<std::size_t,std::size_t>> restrictions_)const
+bool sliceMatrix<Elem,dim>::validRestrictions(std::initializer_list<std::pair<std::ptrdiff_t,std::ptrdiff_t>> restrictions_)const
 {
     if (restrictions_.size()==dim)
     {
@@ -102,6 +102,23 @@ bool sliceMatrix<Elem,dim>::validSteps(std::initializer_list<std::size_t> steps)
     }
     else
         return false;
+}
+
+template<typename Elem,std::size_t dim>
+bool sliceMatrix<Elem,dim>::validAccess(std::size_t dimension,std::ptrdiff_t index)const
+{
+    return Mat.validIndex(dimension,index) && this->inStepRange(dimension,index);
+}
+
+template<typename Elem,std::size_t dim>
+bool sliceMatrix<Elem,dim>::inStepRange(std::size_t dimension,std::ptrdiff_t index)const
+{
+    for (std::ptrdiff_t i=restrictions[dimension].first;i<restrictions[dimension].second;i+=steps[dimension])
+    {
+        if (i==index)
+            return true;
+    }
+    return false;
 }
 
 template<typename Elem,std::size_t dim>
--- sliceMatrix.hpp
+++ sliceMatrix.hpp
@@ -14,14 +14,16 @@ class sliceMatrix
 {
     private:
         Matrix<Elem,dim>& Mat;
-        std::pair<std::size_t,std::size_t>* restrictions;
+        std::pair<std::ptrdiff_t,std::ptrdiff_t>* restrictions;
         std::size_t* steps;
-        bool validRestrictions(std::initializer_list<std::pair<std::size_t,std::size_t>>)const;
+        bool validRestrictions(std::initializer_list<std::pair<std::ptrdiff_t,std::ptrdiff_t>>)const;
         bool validSteps(std::initializer_list<std::size_t>)const;
+        bool validAccess(std::size_t,std::ptrdiff_t)const;
+        bool inStepRange(std::size_t,std::ptrdiff_t)const;
     public:
         //sliceMatrix(Matrix<Elem,dim>&);
-        sliceMatrix(Matrix<Elem,dim>&,std::initializer_list<std::pair<std::size_t,std::size_t>>);
-        sliceMatrix(Matrix<Elem,dim>&,std::initializer_list<std::pair<std::size_t,std::size_t>>,std::initializer_list<std::size_t>);
+        sliceMatrix(Matrix<Elem,dim>&,std::initializer_list<std::pair<std::ptrdiff_t,std::ptrdiff_t>>);
+        sliceMatrix(Matrix<Elem,dim>&,std::initializer_list<std::pair<std::ptrdiff_t,std::ptrdiff_t>>,std::initializer_list<std::size_t>);
         ~sliceMatrix();
 
 };
